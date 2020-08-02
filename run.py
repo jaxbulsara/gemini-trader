13e8d8782e49ded2eb87a6896cbb06fdfc2cd7e9
= <jaxbulsara@gmail.com>
2020-08-02 14:30:54 -0400

    Updated running conditions

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from setup_logging import setup_logging
 
 if __name__ == "__main__":
     start_datetime = datetime.now().strftime("%Y-%m-%S_%H-%M-%S-%f")
-    setup_logging(start_datetime, file=False, debug=True)
+    setup_logging(start_datetime)
 
     trader = GeminiTrader()
     trader.run()
