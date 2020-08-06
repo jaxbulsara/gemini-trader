ece87b509af76d656c86f6750539b54c3acd6e7d
= <jaxbulsara@gmail.com>
2020-08-06 10:03:24 -0400

    Resolved conflicts in run.py

--- run.py
+++ run.py
@@ -5,11 +5,7 @@ from datetime import datetime
 from setup_logging import setup_logging
 
 if __name__ == "__main__":
-<<<<<<< Updated upstream
     start_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
-=======
-    start_datetime = datetime.now().strftime("%Y-%m-%S_%H-%M-%S-%f")
->>>>>>> Stashed changes
     setup_logging(start_datetime)
 
     from GeminiTrader import GeminiTrader
