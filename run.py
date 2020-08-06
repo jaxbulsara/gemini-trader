5f71d662d4786d4fb4d90d69a95f8968e8d9eae6
Jay Bulsara <jaxbulsara@gmail.com>
2020-08-06 09:59:16 -0400

    Updated run.py

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ from datetime import datetime
 from setup_logging import setup_logging
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     start_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
-    setup_logging(start_datetime, debug=True, file=False)
+=======
+    start_datetime = datetime.now().strftime("%Y-%m-%S_%H-%M-%S-%f")
+>>>>>>> Stashed changes
+    setup_logging(start_datetime)
 
     from GeminiTrader import GeminiTrader
 
